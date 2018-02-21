76af1730c753da0ec056a5f4966a3cd17453e459
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-02-21 16:20:57 +0530

    multiple.c
    good

---
+++ multiple.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+printf("\n Please the Value of N:  ");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("\n  %ld = %ld",i);
+}
+getch();
+}
